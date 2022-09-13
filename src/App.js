1a6af5e285f357f3258717f3e026570341f59a9d
Sahilkumar26244 <kumarsahil8981@gmail.com>
2022-09-14 00:11:52 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-// import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./index.js"
+import { Home } from "./routes/Home.js";
+import { About } from "./routes/About.js";
+import { Contact } from "./routes/Contact.js";
+import { Project } from "./routes/Project.js";
+import { Route, Routes } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      hello world
-      sahil kumar
-    </div>
+    <>
+     <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/contact" element={<Contact/>} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/project" element={<Project/>} />
+     </Routes>
+    </>
   );
 }
 
