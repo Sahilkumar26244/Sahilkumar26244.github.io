20f04d53f23b8a14a4daf42817b50b34bab2fe3f
Sahilkumar26244 <kumarsahil8981@gmail.com>
2022-11-05 23:09:49 +0530

    update project section

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -7,14 +7,14 @@ const WorkCardData = [
     {
         imgsrc:pro1,
         title:"Ecommerce Website Design",
-        text:"Amazon.com is a vast Internet-based enterprise that sells books, music, movies, housewares, electronics, toys, and many other goods, either directly or as the middleman between other retailers and Amazon.com's millions of customers.",
+        text:"Amazon.com is a vast Internet-based enterprise that sells books, music, movies, housewares, electronics, toys, and many other goods.",
         view:"https://legendary-sprite-7718cd.netlify.app/",
         source:"https://github.com/Sahilkumar26244/Amazon2"
     },
     {
         imgsrc:pro2,
         title:"Myntra Website Design",
-        text:"Myntra is a one stop shop for all your fashion and lifestyle needs. Being India's largest e-commerce store for fashion and lifestyle products, Myntra aims at providing a hassle free and enjoyable shopping experience to shoppers across the country with the widest range of brands and products on its portal.",
+        text:"Myntra is a one stop shop for all your fashion and lifestyle needs. Being India's largest e-commerce store for fashion and lifestyle products.",
         view:"https://charming-raindrop-77cff4.netlify.app/",
         source:"https://github.com/Sahilkumar26244/acceptable-coil-4935"
     },
